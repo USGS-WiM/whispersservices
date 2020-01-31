ba95924118aabe4596864edf9b1596bbfe045112
Aaron Stephenson <astephenson@usgs.gov>
2020-01-31 16:15:37 -0600

    Finish implementing standard nightly notifications (untested); update dependencies

--- whispersservices/scheduled_tasks.py
+++ whispersservices/scheduled_tasks.py
@@ -5,18 +5,38 @@ from whispersservices.immediate_tasks import generate_notification
 
 
 @shared_task()
-def all_events():
+def standard_notifications():
     yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+    new_events = Event.objects.filter(created_date=yesterday)
+    updated_events = Event.objects.filter(modified_date=yesterday).exclude(created_date=yesterday)
 
-    events_created_yesterday = Event.objects.filter(created_date=yesterday)
+    all_events = all_events(events_created_yesterday=new_events, events_updated_yesterday=updated_events)
+    own_events = own_events(events_created_yesterday=new_events, events_updated_yesterday=updated_events)
+    org_events = organization_events(events_created_yesterday=new_events, events_updated_yesterday=updated_events)
+    collab_events = collaborator_events(events_created_yesterday=new_events, events_updated_yesterday=updated_events)
+
+    all_notifications = all_events + own_events + org_events + collab_events
+    unique_notifications = []
+
+    for notification in all_notifications:
+        if (notification[0], notification[2]) not in unique_notifications:
+            generate_notification.delay(notification)
+
+    return True
+
+
+@shared_task()
+def all_events(events_created_yesterday, events_updated_yesterday):
+    notifications = []
+    msg_tmp = NotificationMessageTemplate.objects.filter(name='ALL Events').first()
+
     standard_notification_cues_new = NotificationCueStandard.objects.filter(
         standard_type__name='All', notification_cue_preference__create_when_new=True)
-    for cue in standard_notification_cues_new:
-        send_email = cue.notification_cue_preference.send_email
-        for event in events_created_yesterday:
+    for event in events_created_yesterday:
+        for cue in standard_notification_cues_new:
+            send_email = cue.notification_cue_preference.send_email
             recipients = [cue.created_by.id, ]
             email_to = [cue.created_by.email, ] if send_email else []
-            msg_tmp = NotificationMessageTemplate.objects.filter(name='Event Created').first()
 
             event_location = "["
             for evtloc in event.eventlocations:
@@ -27,19 +47,19 @@ def all_events():
             subject = msg_tmp.subject_template.format(event_id=event.id)
             body = msg_tmp.body_template.format(
                 event_id=event.id, organization=event.created_by.organization.name, event_location=event_location,
-                event_date=event.created_date, new_updated="New", created_updated="created", updates="")
+                event_date=event.created_date, new_updated="New", created_updated="created", updates="N/A")
             source = event.created_by.username
-            generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+            notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+            # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
 
-    events_updated_yesterday = Event.objects.filter(modified_date=yesterday).exclude(created_date=yesterday)
     standard_notification_cues_updated = NotificationCueStandard.objects.filter(
         standard_type__name='All', notification_cue_preference__create_when_modified=True)
-    for cue in standard_notification_cues_updated:
-        send_email = cue.notification_cue_preference.send_email
-        for event in events_updated_yesterday:
+    for event in events_updated_yesterday:
+        for cue in standard_notification_cues_updated:
+            send_email = cue.notification_cue_preference.send_email
             recipients = [cue.created_by.id, ]
             email_to = [cue.created_by.email, ] if send_email else []
-            msg_tmp = NotificationMessageTemplate.objects.filter(name='Event Updated').first()
+
             event_location = "["
             for evtloc in event.eventlocations:
                 event_location += evtloc.administrative_level_two.name
@@ -52,6 +72,150 @@ def all_events():
                 event_id=event.id, organization=event.created_by.organization.name, event_location=event_location,
                 event_date=event.modified_date, new_updated="Updated", created_updated="updated", updates=updates)
             source = event.modified_by.username
-            generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+            notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+            # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
 
-    return True
+    return notifications
+
+
+@shared_task()
+def own_events(events_created_yesterday, events_updated_yesterday):
+    notifications = []
+    msg_tmp = NotificationMessageTemplate.objects.filter(name='Your Events').first()
+
+    standard_notification_cues_new = NotificationCueStandard.objects.filter(
+        standard_type__name='Own', notification_cue_preference__create_when_new=True)
+    for event in events_created_yesterday:
+        for cue in standard_notification_cues_new:
+            if cue.created_by.id == event.created_by.id:
+                send_email = cue.notification_cue_preference.send_email
+                recipients = [cue.created_by.id, ]
+                email_to = [cue.created_by.email, ] if send_email else []
+
+                subject = msg_tmp.subject_template.format(event_id=event.id)
+                body = msg_tmp.body_template.format(
+                    first_name=event.created_by.first_name, last_name=event.created_by.last_name,
+                    created_updated="created", event_id=event.id, event_date=event.created_date, updates="N/A",
+                    new_updated="New")
+                source = event.created_by.username
+                notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+                # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+
+    standard_notification_cues_updated = NotificationCueStandard.objects.filter(
+        standard_type__name='Own', notification_cue_preference__create_when_modified=True)
+    for event in events_updated_yesterday:
+        for cue in standard_notification_cues_updated:
+            if cue.created_by.id == event.created_by.id:
+                send_email = cue.notification_cue_preference.send_email
+                recipients = [cue.created_by.id, ]
+                email_to = [cue.created_by.email, ] if send_email else []
+
+                updates = ""
+                subject = msg_tmp.subject_template.format(event_id=event.id)
+                body = msg_tmp.body_template.format(
+                    first_name=event.created_by.first_name, last_name=event.created_by.last_name,
+                    created_updated="updated", event_id=event.id, event_date=event.created_date, updates=updates,
+                    new_updated="Updated")
+                source = event.modified_by.username
+                notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+                # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+
+    return notifications
+
+
+@shared_task()
+def organization_events(events_created_yesterday, events_updated_yesterday):
+    notifications = []
+    msg_tmp = NotificationMessageTemplate.objects.filter(name='Organization Events').first()
+
+    standard_notification_cues_new = NotificationCueStandard.objects.filter(
+        standard_type__name='Organization', notification_cue_preference__create_when_new=True)
+    for event in events_created_yesterday:
+        for cue in standard_notification_cues_new:
+            if cue.created_by.organization.id == event.created_by.organization.id:
+                send_email = cue.notification_cue_preference.send_email
+                recipients = [cue.created_by.id, ]
+                email_to = [cue.created_by.email, ] if send_email else []
+
+                subject = msg_tmp.subject_template.format(event_id=event.id)
+                body = msg_tmp.body_template.format(
+                    first_name=event.created_by.first_name, last_name=event.created_by.last_name,
+                    created_updated="created", event_id=event.id, event_date=event.created_date, updates="N/A",
+                    new_updated="New")
+                source = event.created_by.username
+                notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+                # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+
+    standard_notification_cues_updated = NotificationCueStandard.objects.filter(
+        standard_type__name='Organization', notification_cue_preference__create_when_modified=True)
+    for event in events_updated_yesterday:
+        for cue in standard_notification_cues_updated:
+            if cue.created_by.organization.id == event.created_by.organization.id:
+                send_email = cue.notification_cue_preference.send_email
+                recipients = [cue.created_by.id, ]
+                email_to = [cue.created_by.email, ] if send_email else []
+
+                updates = ""
+                subject = msg_tmp.subject_template.format(event_id=event.id)
+                body = msg_tmp.body_template.format(
+                    first_name=event.created_by.first_name, last_name=event.created_by.last_name,
+                    created_updated="updated", event_id=event.id, event_date=event.created_date, updates=updates,
+                    new_updated="Updated")
+                source = event.modified_by.username
+                notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+                # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+
+    return notifications
+
+
+@shared_task()
+def collaborator_events(events_created_yesterday, events_updated_yesterday):
+    notifications = []
+    msg_tmp = NotificationMessageTemplate.objects.filter(name='Collaborator Events').first()
+
+    standard_notification_cues_new = NotificationCueStandard.objects.filter(
+        standard_type__name='Collaborator', notification_cue_preference__create_when_new=True)
+    for event in events_created_yesterday:
+        event_collaborator_ids = set(list(User.objects.filter(
+            Q(eventwriteusers__event_id=event.id) |
+            Q(eventreadusers__event_id=event.id)
+        ).values_list('id', flat=True)))
+        for cue in standard_notification_cues_new:
+            if cue.created_by.organization.id in event_collaborator_ids:
+                send_email = cue.notification_cue_preference.send_email
+                recipients = [cue.created_by.id, ]
+                email_to = [cue.created_by.email, ] if send_email else []
+
+                subject = msg_tmp.subject_template.format(event_id=event.id)
+                body = msg_tmp.body_template.format(
+                    first_name=event.created_by.first_name, last_name=event.created_by.last_name,
+                    created_updated="created", event_id=event.id, event_date=event.created_date, updates="N/A",
+                    new_updated="New")
+                source = event.created_by.username
+                notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+                # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+
+    standard_notification_cues_updated = NotificationCueStandard.objects.filter(
+        standard_type__name='Collaborator', notification_cue_preference__create_when_modified=True)
+    for event in events_updated_yesterday:
+        event_collaborator_ids = set(list(User.objects.filter(
+            Q(eventwriteusers__event_id=event.id) |
+            Q(eventreadusers__event_id=event.id)
+        ).values_list('id', flat=True)))
+        for cue in standard_notification_cues_updated:
+            if cue.created_by.organization.id in event_collaborator_ids:
+                send_email = cue.notification_cue_preference.send_email
+                recipients = [cue.created_by.id, ]
+                email_to = [cue.created_by.email, ] if send_email else []
+
+                updates = ""
+                subject = msg_tmp.subject_template.format(event_id=event.id)
+                body = msg_tmp.body_template.format(
+                    first_name=event.created_by.first_name, last_name=event.created_by.last_name,
+                    created_updated="updated", event_id=event.id, event_date=event.created_date, updates=updates,
+                    new_updated="Updated")
+                source = event.modified_by.username
+                notifications.append((recipients, source, event.id, 'event', subject, body, send_email, email_to))
+                # generate_notification.delay(recipients, source, event.id, 'event', subject, body, send_email, email_to)
+
+    return notifications
